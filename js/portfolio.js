2c4efc2605dd76b6fe16842278c5d1fefd952488
Oliver Zhao <oz@Olivers-MacBook-Pro.local>
2016-01-01 00:37:54 -0500

    added modal

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,7 @@
 /*globals portfolioData*/
 'use strict';
 var loadPortfolio = function() {
+  //nav bar
   $('li').mouseenter(function() {
     $(this).addClass('active');
   });
@@ -8,25 +9,25 @@ var loadPortfolio = function() {
     $(this).removeClass('active');
   });
 
+  //portfolio items
   //if change ITEMS_PER_ROW, also change col-md-4 to something else
   var ITEMS_PER_ROW = 3;
-  var createItem = function(name, imgSrc, description) {
-return `<div class="col-md-4 portfolio-item">
-    <a href="#">
+  var createItem = function(index, name, imgSrc, description) {
+return `<div class="col-md-4 portfolio-item" id="` + index +  `">
+    <a class="portfolio-item-image">
         <img class="img-responsive" src=` + imgSrc + ` alt="">
     </a>
-    <h3>
-        <a href="#">` + name + `</a>
-    </h3>
-    <p>` + description +`</p>
+    <div class="portfolio-short-description">
+      <p>` + description +`</p>
+    </div>
 </div>`
   };
 
   var createRow = function(content) {
     $('#content').append('<div class="row">' + content + '</div>');
   };
-  var items = portfolioData.map(function(item) {
-    return createItem(item.name, item.img, item.description);
+  var items = $.map(portfolioData, function(item, index) {
+    return createItem(index, item.name, item.img, item.description);
   });
   var counter = 0;
   var content = "";
@@ -41,4 +42,20 @@ return `<div class="col-md-4 portfolio-item">
   if (counter !== ITEMS_PER_ROW) {
     createRow(content);
   }
+
+  //onclick portfolio items, modal w/ information
+  $('.portfolio-item-image').mouseenter(function() {
+    $(this).parent().find('.portfolio-short-description').addClass('hover-bg');
+  });
+  $('.portfolio-item-image').mouseout(function() {
+    $(this).parent().find('.portfolio-short-description').removeClass('hover-bg');
+  });
+  $('.portfolio-item').click(function() {
+    $('#my-modal').modal('show');
+    var id = $(this).attr('id');
+    // console.log(portfolioData[id]);
+    var data = portfolioData[id];
+    $('#my-modal-title').text(data.name);
+    $('#modal-body-text').text(data.description);
+  });
 };

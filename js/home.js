a06b3fd467863e4375c074d03fcd82f345a97934
Oliver Zhao <oz@olivers-mbp.risd.edu>
2016-01-21 19:57:28 -0500

    added images to portfolio and changed logo

--- js/home.js
+++ js/home.js
@@ -4,19 +4,4 @@ var loadHome = function() {
   var changeImg = function(src) {
     $("#logo-home").attr('src', src)
   };
-  // $('.btn').mouseenter(function() {
-  //   var buttonText = $(this).context.childNodes[0].nodeValue;
-  //   if (buttonText === 'About') {
-  //     changeImg('img/placeholder1.png');
-  //   }
-  //   if (buttonText === 'Portfolio') {
-  //     changeImg('img/placeholder2.png');
-  //   }
-  //   if (buttonText === 'Contact') {
-  //     changeImg('img/placeholder3.png');
-  //   }
-  // });
-  $('.btn').mouseout(function() {
-    changeImg('img/logo.png');
-  });
 };
--- js/portfolio.js
+++ js/portfolio.js
@@ -14,11 +14,9 @@ var loadPortfolio = function() {
   var ITEMS_PER_ROW = 3;
   var createItem = function(index, name, imgSrc, description) {
 return `<div class="col-md-4 portfolio-item" id="` + index +  `">
-    <a class="portfolio-item-image">
-        <img class="img-responsive" src=` + imgSrc + ` alt="">
-    </a>
-    <div class="portfolio-short-description">
-      <p>` + description +`</p>
+    <img class="img-responsive portfolio-item-image" src=` + imgSrc + ` alt="">
+    <div class="portfolio-preview-title">
+      <p>` + name +`</p>
     </div>
 </div>`
   };
@@ -27,35 +25,46 @@ return `<div class="col-md-4 portfolio-item" id="` + index +  `">
     $('#content').append('<div class="row">' + content + '</div>');
   };
   var items = $.map(portfolioData, function(item, index) {
-    return createItem(index, item.name, item.img, item.description);
+    return createItem(index, item.name, item.imgs[0], item.desription);
   });
   var counter = 0;
-  var content = "";
+  var content = '';
   for (var i = 0; i < items.length; ++i) {
+    ++counter;
+    content += items[i];
     if (counter === ITEMS_PER_ROW) {
       counter = 0;
       createRow(content);
-      content = "";
+      content = '';
     }
-    content += items[i];
   }
-  if (counter !== ITEMS_PER_ROW) {
+  if (counter !== 0) {
     createRow(content);
   }
 
   //onclick portfolio items, modal w/ information
   $('.portfolio-item-image').mouseenter(function() {
-    $(this).parent().find('.portfolio-short-description').addClass('hover-bg');
+    $(this).parent().find('.portfolio-preview-title').addClass('hover-bg');
   });
   $('.portfolio-item-image').mouseout(function() {
-    $(this).parent().find('.portfolio-short-description').removeClass('hover-bg');
+    $(this).parent().find('.portfolio-preview-title').removeClass('hover-bg');
   });
   $('.portfolio-item').click(function() {
-    $('#my-modal').modal('show');
+    $('#portfolio-modal').modal('show');
     var id = $(this).attr('id');
     // console.log(portfolioData[id]);
     var data = portfolioData[id];
-    $('#my-modal-title').text(data.name);
-    $('#modal-body-text').text(data.description);
+    $('#portfolio-modal-title').text(data.name);
+    $('#modal-body-text').multiline(data.description);
+    for (var i = 1; i < data.imgs.length; ++i) {
+      $('#modal-body-imgs').append('<div class="row-centered"><img class="modal-img" src="' + data.imgs[i] + '"></img></div>');
+    }
   });
+
+
+  //empties modal-imgs when closed
+  $('#portfolio-modal').on('hidden.bs.modal', function () {
+    $('#modal-body-imgs').empty();
+  });
+
 };

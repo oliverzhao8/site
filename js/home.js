b5d1a843f85b1578002bb0154f1fb38bd0a437ac
Harrison Zhao <roarofpie@gmail.com>
2016-01-31 00:35:35 -0500

    changed the css for more mobile-friendly headers and the home list

--- js/home.js
+++ js/home.js
@@ -1,7 +1,22 @@
 /* globals loadRoute */
 'use strict';
 var loadHome = function() {
+  var MIN_HOME_WIDTH = 375;
+  var onResize = function() {
+    var btnList = $("#btn-list-home");
+    var listInline = 'list-inline';
+    if ($(window).width() < MIN_HOME_WIDTH) {
+      btnList.removeClass(listInline);
+    } else {
+      btnList.addClass(listInline);
+    }
+  };
+
   var changeImg = function(src) {
     $("#logo-home").attr('src', src);
   };
+
+  //initialize and register handler for resize
+  onResize();
+  $(window).resize(onResize);
 };
